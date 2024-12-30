1867a3e14e77a864320cd43258724fb675af0a84
Zahar <zaharmaticev@gmail.com>
2024-12-30 10:18:13 +0500

    with out cash

--- src/components/Slider/images.js
+++
@@ -1,8 +0,0 @@
-export default function getImages() {
-  const photos = []
-  for (let i = 1; i <= 9; i++) {
-    photos.push(`photo_${i}`)
-  }
-  return photos
-}
-
--- src/utils/handlePopupMessage.jsx
+++
@@ -1,5 +0,0 @@
-import { toast } from "react-toastify"
-
-export default function handlePopupMessage(name, value = "") {
-  return toast(`${name}, ${value}`)
-}
